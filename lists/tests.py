1a8abd908ca276cd72b0f5e07c81bd3abd6cc7bc
Gustavo Luna <marcacacahuate@outlook.com>
2023-04-03 23:57:26 -0600

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -20,20 +20,11 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/list/the-only-list-in-the-world/')
 
     def test_only_save_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-
-    def test_display_all_list_items(self):
-        Item.objects.create(text="itemy 1")
-        Item.objects.create(text="itemy 2")
-
-        response = self.client.get('/')
-
-        self.assertIn('itemy 1', response.content.decode())
-        self.assertIn('itemy 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -54,3 +45,19 @@ class ItemModelTest(TestCase):
         second_item_saved = saved_items[1]
         self.assertEqual(first_item_saved.text, "the first (ever) list item")
         self.assertEqual(second_item_saved.text, "Item the second")
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('list/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_display_all_items(self):
+        Item.objects.create(text="itemy 1")
+        Item.objects.create(text="itemy 2")
+
+        response = self.client.get('/list/the-only-list-in-the-world/')
+
+        self.assertContains(response, 'itemy 1')
+        self.assertContains(response, 'itemy 2')
--- lists/views.py
+++ lists/views.py
@@ -6,7 +6,9 @@ from lists.models import Item
 def home_page(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         Item.objects.create(text=request.POST["item_text"])
-        return redirect('/')
+        return redirect('/list/the-only-list-in-the-world/')
+    return render(request, 'home.html')
 
+def view_list(request: HttpRequest):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
