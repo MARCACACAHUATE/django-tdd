471a703d8d2adca75c74d71347d0a1e126bd54d3
Gustavo Luna <marcacacahuate@outlook.com>
2023-04-29 12:43:59 -0600

    Add csrf trusted origins

--- superlists/settings.py
+++ superlists/settings.py
@@ -125,3 +125,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["https://django-tdd-production.up.railway.app"]
